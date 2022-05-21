00f6bea8782eab3f2712591a72bcd7c7fb9c4b86
david <davidmq09@gmail.com>
2022-05-21 18:59:35 -0300

    agrego Ejercicio2 en branch CalculadoraV1

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,32 +1,65 @@
-﻿Console.WriteLine("==== Ejercicio 1 ====");
+﻿Console.WriteLine("==== Ejercicio 2 - CalculadoraV1 ====");
 
-Console.WriteLine("Ingrese el número a invertir (mayor a 0): ");
-int numero = Convert.ToInt32(Console.ReadLine());
+int operacion, num1, num2, repetir;
 
-if (numero > 0)
+do
 {
-    int numeroInvertido = 0, ultimoDigito;
-    while (numero > 0)
+    Console.WriteLine("Ingrese la operación que desea realizar (1: sumar, 2: restar, 3: multiplicar, 4: dividir)");
+    operacion = Convert.ToInt32(Console.ReadLine());   
+    
+    Console.WriteLine("Ingrese el primer número: ");
+    num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Ingrese el segundo número: ");
+    num2 = Convert.ToInt32(Console.ReadLine()); 
+
+    if (operacion == 4)
     {
-        ultimoDigito = numero % 10;
-        numeroInvertido = (numeroInvertido * 10) + ultimoDigito;
-        numero = numero / 10;
+        while (num2 == 0)
+        {
+            Console.WriteLine("ERROR! el divisor no puede ser 0");
+            Console.WriteLine("Ingrese nuevamente el divisor: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+        }
     }
-    Console.WriteLine("El número invertido es: " + numeroInvertido);
-}
-else
+    
+    switch (operacion)
+    {
+        case 1:
+            Console.WriteLine($"El resultado de la suma es: {sumar(num1, num2)}");
+            break;
+        case 2:
+            Console.WriteLine($"El resultado de la resta es: {restar(num1, num2)}");
+            break;
+        case 3:
+            Console.WriteLine($"El resultado del producto es: {multiplicar(num1, num2)}");
+            break;
+        case 4:
+            Console.WriteLine($"El resultado de la división es: {dividir(num1, num2)}");
+            break;
+    }
+
+    Console.WriteLine("Desea realizar otro cálculo? (1: Si, 0: No)");
+    repetir = Convert.ToInt32(Console.ReadLine());
+
+} while (repetir == 1);
+
+
+int sumar(int a, int b)
 {
-    Console.WriteLine("La conversión no se realizó porque el número ingresado NO es mayor a 0");
+    return a + b;
 }
 
+int restar(int a, int b)
+{
+    return a - b;
+}
 
-/* otra forma: tomar el número como una cadena y luego invertirlo
+int multiplicar(int a, int b)
+{
+    return a * b;
+}
 
-Console.WriteLine("Ingrese el numero a invertir (mayor a 0): ");
-char[] array = Console.ReadLine().ToCharArray();
-
-Array.Reverse(array);
-
-string numeroInvertido = new string(array);
-
-Console.WriteLine("El numer invertido es: " + numeroInvertido); */
+int dividir(int a, int b)
+{
+    return a / b;
+}
