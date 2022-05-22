e5446ae798db287a5d7b2f42885e034740360c04
david <davidmq09@gmail.com>
2022-05-21 22:09:28 -0300

    agrego branch V2  con mejoras para la V1

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,32 +1,103 @@
-﻿Console.WriteLine("==== Ejercicio 1 ====");
+﻿Console.WriteLine("CalculadoraV2 (Mejoras para la V1) ====");
 
-Console.WriteLine("Ingrese el número a invertir (mayor a 0): ");
-int numero = Convert.ToInt32(Console.ReadLine());
+int operacion;
+double num1, num2;
 
-if (numero > 0)
+Console.WriteLine("Ingrese la operación que desea realizar:");
+Console.WriteLine("[5] valor absoluto, [6] cuadrado, [7] raíz cuadrada, [8] seno, [9] coseno, [10] parte entera de un float)");
+operacion = Convert.ToInt32(Console.ReadLine());   
+
+Console.WriteLine("Ingrese el número a operar: ");
+num1 = Convert.ToDouble(Console.ReadLine());
+
+if (operacion == 7)
 {
-    int numeroInvertido = 0, ultimoDigito;
-    while (numero > 0)
+    while (num1 < 0)
     {
-        ultimoDigito = numero % 10;
-        numeroInvertido = (numeroInvertido * 10) + ultimoDigito;
-        numero = numero / 10;
+        Console.WriteLine("ERROR! La raíz cuadrada de un número negativo no existe");
+        Console.WriteLine("Ingrese nuevamente el número: ");
+        num1 = Convert.ToDouble(Console.ReadLine());
     }
-    Console.WriteLine("El número invertido es: " + numeroInvertido);
 }
-else
+
+switch (operacion)
 {
-    Console.WriteLine("La conversión no se realizó porque el número ingresado NO es mayor a 0");
+    case 5:
+        Console.WriteLine($"El valor absoluto es: {valorAbsoluto(num1)}");
+        break;
+    case 6:
+        Console.WriteLine($"El cuadrado es: {cuadrado(num1)}");
+        break;
+    case 7:
+        Console.WriteLine($"La raíz cuadrada es: {raizCuadrada(num1)}");
+        break;
+    case 8:
+        Console.WriteLine($"El seno es: {seno(num1)}");
+        break;
+    case 9:
+        Console.WriteLine($"El coseno es: {coseno(num1)}");
+        break;
+    case 10:
+        Console.WriteLine($"La parte entera es: {parteEntera(num1)}");
+        break;
+}
+
+Console.WriteLine("Ingrese la operación que desea realizar:");
+Console.WriteLine("[11] el máximo entre 2 números, [12] el mínimo entre 2 números");
+operacion = Convert.ToInt32(Console.ReadLine()); 
+
+Console.WriteLine("Ingrese el primer número:");
+num1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo número:");
+num2 = Convert.ToDouble(Console.ReadLine());
+    
+switch (operacion)
+{
+    case 11:
+        Console.WriteLine($"El máximo entre los números es: {maximo(num1, num2)}");
+        break;
+    case 12:
+        Console.WriteLine($"El mínimo entre los números es: {minimo(num1, num2)}");
+        break;
 }
 
 
-/* otra forma: tomar el número como una cadena y luego invertirlo
+double valorAbsoluto(double a)
+{
+    return Math.Abs(a);
+}
 
-Console.WriteLine("Ingrese el numero a invertir (mayor a 0): ");
-char[] array = Console.ReadLine().ToCharArray();
+double cuadrado(double a)
+{
+    return Math.Pow(a,2);
+}
 
-Array.Reverse(array);
+double raizCuadrada(double a)
+{
+    return Math.Sqrt(a);
+}
 
-string numeroInvertido = new string(array);
+double seno(double a)
+{
+    return Math.Sin(a);
+}
 
-Console.WriteLine("El numer invertido es: " + numeroInvertido); */
+double coseno(double a)
+{
+    return Math.Cos(a);
+}
+
+double parteEntera(double a)
+{
+    return Math.Truncate(a);
+}
+
+double maximo(double a, double b)
+{
+    return Math.Max(a, b);
+}
+
+double minimo(double a, double b)
+{
+    return Math.Min(a, b);
+}
